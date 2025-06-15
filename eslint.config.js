e15e03e6c281e0d9aca9aa15c30e50d5fb76e364
ZakariaBatty <zbatty1297@gmail.com>
2025-06-15 13:18:20 +0100

    feat: add ESLint configuration file for Next.js with custom rules

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+export default [
+  ...compat.config({
+    extends: ['next/core-web-vitals'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
